5bb20468952ffc4329f8bb48628034eb3f49ca79
dsz1234 <2634164883@qq.com>
2023-10-03 12:57:15 +0800

    feat(第一次): 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bg-[url('assets/imgs/bg.jpg')] bg-cover bg-center h-screen text-white p-5 flex overflow-hidden">
+    <!-- 左 -->
+    <div class=" flex-1"></div>
+    <!-- 中 -->
+    <div></div>
+    <!-- 右 -->
+    <div></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
